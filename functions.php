ab537593dad12c4104502ccbea370c9692c564ae
subhanghani339 <subhanghani339@gmail.com>
2022-06-22 11:39:58 +0500

    asdf

---
+++ functions.php
@@ -0,0 +1,8 @@
+<?php
+
+register_nav_menus(
+    array("primary-menu" => "Top Menu")
+)
+
+
+?>
---
+++ header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>IIP</title>
+    <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/style.css" />
+</head>
+<body> 
+    <div>
+        <?php wp_nav_menu(
+            array(
+            'theme_location'=>'primary-menu'
+            ))?>
+    </div>
+
